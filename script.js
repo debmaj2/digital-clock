f25c0719489b2604a0377a3e4821cd3241ee810d
Debosmit Majumder <majumderdebosmit@gmail.com>
2020-06-09 00:21:24 +0530

    Changed Font Colour to Red from Black

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function get_time() {
     const nightBg = '#2a3444';
     const nightText = '#fadb44';
     const dayBg = '#d4d2d4';
-    const dayText = '#000000';
+    const dayText = '#FF0000';
   
     var hour = new Date().getHours();
     if([18,19,20,21,22,23,0,1,2,3,4,5,6].includes(hour)) {
